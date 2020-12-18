deeee20db8fdc67343ae9ffd6c15856d5a14cbd5
GamesDevDave <gamesdevdave@gmail.com>
2020-12-18 08:47:56 +0000

    - Uploading project. - Worked on creating basic button. - Ensured project is ready for future development.

---
+++ UIWorksheet-Comp280/Assets/Scripts/SCR_UIInteraction.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SCR_UIInteraction : MonoBehaviour
+{
+    // Used whenever the play button has been pressed by the user.
+        public void OnPlayPress() => SceneManager.LoadScene("SCN_MainGameScene", LoadSceneMode.Single);
+}
