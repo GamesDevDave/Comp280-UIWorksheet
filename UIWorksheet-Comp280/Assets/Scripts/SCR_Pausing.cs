084aeb7a126464178fceef49da7ffff86b3bbbf0
GamesDevDave <gamesdevdave@gmail.com>
2020-12-18 11:44:50 +0000

    - Added the remaining scenes. - Started to add code relating to the Pause Menu.

---
+++ UIWorksheet-Comp280/Assets/Scripts/SCR_Pausing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Pausing : MonoBehaviour
+{
+    
+    private SCR_UIInteraction uiScript;
+
+    private bool isPaused = false;
+
+    private void Start() 
+    {
+        uiScript = GetComponent<SCR_UIInteraction>();
+    }
+    public void CheckForPause()
+    {
+        if(uiScript.pausePressed)
+        {
+            if(isPaused)
+            {
+                Time.timeScale = 0;
+                Debug.Log("Testing Pause");
+            }
+
+            else
+            {
+                Time.timeScale = 1;
+                Debug.Log("Testing Resume");
+            }
+        }
+    }
+}
--- UIWorksheet-Comp280/Assets/Scripts/SCR_UIInteraction.cs
+++ UIWorksheet-Comp280/Assets/Scripts/SCR_UIInteraction.cs
@@ -3,6 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class SCR_UIInteraction : MonoBehaviour
 {
+
+    [Tooltip("Script that has the pausing related code.")]
+    private SCR_Pausing pausingScript;
+    public bool pausePressed;
+
+    private void Start() 
+    {
+        pausingScript = GetComponent<SCR_Pausing>();
+    }
+
     // Used whenever the play button has been pressed by the user.
-        public void OnPlayPress() => SceneManager.LoadScene("SCN_MainGameScene", LoadSceneMode.Single);
+    public void OnPlayPress() => SceneManager.LoadScene("SCN_MainGameScene", LoadSceneMode.Single);
+    public void OnOptionsPress() => SceneManager.LoadScene("SCN_OptionsScene", LoadSceneMode.Single);
+    public void OnControlsPress() => SceneManager.LoadScene("SCN_ControlsScene", LoadSceneMode.Single);
+    public void OnMainMenuPress() => SceneManager.LoadScene("SCN_MainMenu", LoadSceneMode.Single);
+    public void OnQuitPress() => Application.Quit();
+
+    public void OnPausePress()
+    {
+        pausePressed = true;
+        pausingScript.CheckForPause();
+    }
+
+    private void Update() 
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnPausePress();
+        }
+    }
 }
